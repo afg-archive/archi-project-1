a70595858aab8164ae4f50e70f0aad89b7f42d83
afg <afg984@gmail.com>
2016-03-19 19:57:59 +0800

    move word.cc into word.h

--- 103033110_01/simulator/src/word.h
+++ 103033110_01/simulator/src/word.h
@@ -5,10 +5,14 @@
 
 struct Word {
     union {
-        uint32_t u = 0u;  // signed value
+        uint32_t u;  // signed value
         int32_t s;   // unsigned value
     };
 
-    uint32_t bits(uint32_t, uint32_t) const;
-    // word[a:b]
+    Word(uint32_t initial = 0u): u(initial) {}
+
+    // word[m:l]
+    uint32_t bits(uint32_t, uint32_t) const {
+        return (this->u >> l) & ((2 << (m - l)) - 1);
+    }
 };
