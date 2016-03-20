aab712473a7fbeca654f72ce62a921f5bceb48ee
afg <afg984@gmail.com>
2016-03-20 12:58:38 +0800

    const QAQ

--- 103033110_01/simulator/src/memory.h
+++ 103033110_01/simulator/src/memory.h
@@ -27,11 +27,11 @@ class Memory {
     private:
         void set_error(Fatal Error) {
             if (mem.esp) {
-                mem.esp->set(Error);
+                mem.esp->fatals.set(Error);
             }
         }
         template <class T>
-        bool check() const {
+        bool check() {
             bool clean = true;
             if (offset + sizeof(T) > mem.Bytes) {
                 clean = false;
@@ -50,7 +50,7 @@ class Memory {
             }
         }
         template <class T>
-        T get() const {
+        T get() {
             if (check<T>()) {
                 return ((T*)mem.data)[offset / sizeof(T)];
             } else {
@@ -61,37 +61,37 @@ class Memory {
         void setu8(uint8_t value) {
             set<uint8_t>(value);
         }
-        uint8_t getu8() const {
+        uint8_t getu8() {
             return get<uint8_t>();
         }
         void sets8(int8_t value) {
             set<int8_t>(value);
         }
-        int8_t gets8() const {
+        int8_t gets8() {
             return get<int8_t>();
         }
         void setu16(uint16_t value) {
             set<uint16_t>(htobe16(value));
         }
-        uint16_t getu16() const {
+        uint16_t getu16() {
             return be16toh(get<uint16_t>());
         }
         void sets16(int16_t value) {
             setu16(type_convert<uint16_t>(value));
         }
-        int16_t gets16() const {
+        int16_t gets16() {
             return type_convert<int16_t>(getu16());
         }
         void setu32(uint32_t value) {
             set<uint32_t>(htobe32(value));
         }
-        uint32_t getu32() const {
+        uint32_t getu32() {
             return be32toh(get<uint32_t>());
         }
         void sets32(int32_t value) {
             setu32(type_convert<uint32_t>(value));
         }
-        int32_t gets32() const {
+        int32_t gets32() {
             return type_convert<int32_t>(getu32());
         }
     };
