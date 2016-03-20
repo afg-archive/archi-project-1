6bb80d9dba56b71986ebdf5ac971cca8072a7867
afg <afg984@gmail.com>
2016-03-20 15:17:37 +0800

    simulator tests

--- 103033110_01/simulator/src/simulator.h
+++ 103033110_01/simulator/src/simulator.h
@@ -1,6 +1,7 @@
 #include <endian.h>
 #include <iostream>
 #include <exception>
+#include <stdexcept>
 #include <algorithm>
 #include "memory.h"
 #include "errors.h"
@@ -11,7 +12,9 @@ uint32_t load_bigendian(std::istream& is) {
         char buf[4];
         uint32_t value;
     } temp;
-    is.read(temp.buf, 4);
+    if (not is.read(temp.buf, 4)) {
+        throw std::runtime_error("Cannot read bytes from stream");
+    };
     return be32toh(temp.value);
 }
 
@@ -30,14 +33,14 @@ public:
     std::ostream& errorhere;
     std::ostream& loghere;
     Simulator(
-        std::ostream& dumphere,
-        std::ostream& errorhere,
+        std::ostream& dumphere = std::cout,
+        std::ostream& errorhere = std::cout,
         std::ostream& loghere = std::cout):
         pc(0),
         cycle_count(0),
         imem(nullptr),
         dmem(nullptr),
-        rmem(new RMemory(32)),
+        rmem(new RMemory(32 * 4)),
         dumphere(dumphere),
         errorhere(errorhere),
         loghere(loghere)
@@ -66,8 +69,8 @@ public:
         load_imem(size, is);
     }
     void load_sp(std::istream& is) {
-        rmem->at(29).setu32(load_bigendian(is));
-        loghere << "SP ($29) initialized to " << rmem->at(29).getu32();
+        rmem->at(29 * 4).setu32(load_bigendian(is));
+        loghere << "SP ($29) initialized to " << rmem->at(29 * 4).getu32() << '\n';
     }
     void load_dmem(size_t size, std::istream& is) {
         delete dmem;
--- 103033110_01/simulator/src/test_simulator.cc
+++ 103033110_01/simulator/src/test_simulator.cc
@@ -1,2 +1,102 @@
 #include <gtest/gtest.h>
+#include <sstream>
 #include "simulator.h"
+
+
+using namespace std;
+
+
+TEST(Simulator, LoadPC) {
+    istringstream iss("\xf2\x56\x9a\xde");
+    Simulator sim;
+    sim.load_pc(iss);
+
+    ASSERT_EQ(0xf2569ade, sim.pc);
+}
+
+
+TEST(Simulator, LoadSP) {
+    istringstream iss("\xea\xcd\x43\x99");
+    Simulator sim;
+    sim.load_sp(iss);
+
+    ASSERT_EQ(0xeacd4399, sim.rmem->at(29 * 4).getu32());
+}
+
+
+
+TEST(Simulator, LoadDMemory) {
+    istringstream iss(
+        "\x12\x34\x56\x78"
+        "\x90\xab\xcd\xef"
+    );
+    Simulator sim;
+    sim.load_dmem(8, iss);
+
+    EXPECT_EQ(1024, sim.dmem->Bytes);
+    EXPECT_EQ(0x12345678, sim.dmem->at(0).getu32());
+    EXPECT_EQ(0x90abcdef, sim.dmem->at(4).getu32());
+}
+
+
+TEST(Simulator, LoadIMemory) {
+    istringstream iss(
+        "\x90\xab\xcd\xef"
+        "\x12\x34\x56\x78"
+    );
+    Simulator sim;
+    sim.load_imem(8, iss);
+
+    EXPECT_EQ(8, sim.imem->Bytes);
+    EXPECT_EQ(0x90abcdef, sim.imem->at(0).getu32());
+    EXPECT_EQ(0x12345678, sim.imem->at(4).getu32());
+}
+
+
+TEST(Simulator, LoadDImage) {
+    istringstream iss(
+        string(
+            "\xea\xcd\x43\x99"
+            "\x00\x00\x00\x08"
+            "\x12\x34\x56\x78"
+            "\x90\xab\xcd\xef",
+            16
+        )
+    );
+    Simulator sim;
+    sim.load_dimage(iss);
+
+    EXPECT_EQ(0xeacd4399, sim.rmem->at(29 * 4).getu32());
+    EXPECT_EQ(0x12345678, sim.dmem->at(0).getu32());
+    EXPECT_EQ(0x90abcdef, sim.dmem->at(4).getu32());
+}
+
+
+TEST(Simulator, LoadIImage) {
+    istringstream iss(
+        string(
+            "\xf2\x56\x9a\xde"
+            "\x00\x00\x00\x08"
+            "\x90\xab\xcd\xef"
+            "\x12\x34\x56\x78",
+            16
+        )
+    );
+
+    Simulator sim;
+    sim.load_iimage(iss);
+
+    EXPECT_EQ(0xf2569ade, sim.pc);
+    EXPECT_EQ(0x90abcdef, sim.imem->at(0).getu32());
+    EXPECT_EQ(0x12345678, sim.imem->at(4).getu32());
+}
+
+
+TEST(Simulator, RMemory) {
+    Simulator sim;
+
+    for (uint32_t i = 0; i < 32u; ++ i) {
+        sim.rmem->at(i * 4).setu32(0xe9234927 + i);
+        EXPECT_EQ(0xe9234927 + i, sim.rmem->at(i * 4).getu32());
+    }
+}
