47c0b1bc5e98e9e8f9fb1a6e4bb7340857e7f0b6
afg <afg984@gmail.com>
2016-03-20 21:25:56 +0800

    main

---
+++ 103033110_01/simulator/main.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include "src/simulator.h"
+
+
+using namespace std;
+
+
+int main() {
+    ifstream dimage("dimage.bin", ios::binary);
+    ifstream iimage("iimage.bin", ios::binary);
+    ofstream errordump("error_dump.rpt");
+    ofstream regdump("snapshot.rpt");
+    Simulator sim(regdump, errordump);
+    sim.load_iimage(iimage);
+    sim.load_dimage(dimage);
+    sim.run();
+}
